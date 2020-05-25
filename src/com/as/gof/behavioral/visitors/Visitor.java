66b37c6c2a8b7fccfd19075cae96a6ee9a1641dc
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-05-25 11:18:03 +0300

    Employee, Visitor added

---
+++ src/com/as/gof/behavioral/visitors/Visitor.java
@@ -0,0 +1,13 @@
+package com.as.gof.behavioral.visitors;
+
+/**
+ * Created by Slesarchuk on 05/25/2020.
+ */
+public interface Visitor {
+
+    void visit(Employee employee);
+
+    //void visit(Boss boss);
+    //void visit(SuperBoss superBoss);
+
+}
