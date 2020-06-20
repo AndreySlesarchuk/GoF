fc8305e6b3175c26a8e988423ee6e823da06bc9b
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-06-20 19:27:47 +0300

    Create Digest

---
+++ src/com/as/gof/behavioral/template/authorization/Diges.java
@@ -0,0 +1,26 @@
+package com.as.gof.behavioral.template.authorization;
+
+/**
+ * Created by Andrey Slesarchuk on 06/20/2020.
+ */
+public class Diges {
+  private String digest;
+
+  private int salt;
+
+  public String getDigest() {
+    return digest;
+  }
+
+  public void setDigest(String digest) {
+    this.digest = digest;
+  }
+
+  public int getSalt() {
+    return salt;
+  }
+
+  public void setSalt(int salt) {
+    this.salt = salt;
+  }
+}
--- src/com/as/gof/behavioral/template/authorization/UserStorage.java
+++ src/com/as/gof/behavioral/template/authorization/UserStorage.java
@@ -31,5 +31,4 @@ public class UserStorage {
     return null;
   }
 
-
 }
