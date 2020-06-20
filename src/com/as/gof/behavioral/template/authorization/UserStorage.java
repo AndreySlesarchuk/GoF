2ad823a22c1484302f7e74daf47cc14dcbb350fe
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-06-20 19:24:05 +0300

    Create UserStorageException

--- src/com/as/gof/behavioral/template/authorization/UserStorage.java
+++ src/com/as/gof/behavioral/template/authorization/UserStorage.java
@@ -19,16 +19,7 @@ public class UserStorage {
 
   private List<User> users = new ArrayList<User>();
 
-  public void initialize() {
-    users.add(createPasswordUser ("user1", "login1", "password1"));
-    users.add(createPasswordUser ("user2", "login2", "password2"));
-    users.add(createPasswordUser ("user3", "login3", "password3"));
-    users.add(createPasswordUser ("user4", "login4", "password4"));
-    users.add(createPasswordUser ("user5", "login5", "password5"));
-    users.add(createTokenUser("user6", "login6", 1));
-    users.add(createTokenUser("user7", "login7", 2));
-    users.add(createTokenUser("user8", "login8", 3));
-    users.add(createTokenUser("user9", "login9", 4));
+  public void initialize() throws UserStorageException {
   }
 
   public User findUserByLoginName(String loginName){
@@ -40,29 +31,5 @@ public class UserStorage {
     return null;
   }
 
-  private static TokenUser createTokenUser(String userId, String loginName, int tokenId){
-    TokenUser tokenUser = new TokenUser(userId);
-    tokenUser.setLoginName(loginName);
-    tokenUser.setTokenId(tokenId);
-    return tokenUser;
-  }
 
-  private static PasswordUser createPasswordUser(String userId, String loginName, String password) throws UserStorageException {
-    PasswordUser user = new PasswordUser(userId);
-    Digest digest = createDigest(password);
-    user.setLoginName(loginName);
-    user.setPasswordDigest(digest.getDigest());
-    user.setSalt(digest.getSalt());
-    return user;
-  }
-
-  private static Digest createDigest(String password) throws UserStorageException {
-    try {
-      return Digester.calculateDigest(password);
-    } catch (NoSuchAlgorithmException e) {
-      throw new UserStorageException(e);
-    } catch (UnsupportedEncodingException e) {
-      throw new UserStorageException(e);
-    }
-  }
 }
