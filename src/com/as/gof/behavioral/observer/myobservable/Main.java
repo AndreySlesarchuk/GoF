6f2dca1c38005a50e92777bd9c9eb842a3f8952c
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-07-26 17:16:42 +0300

    Create MyObservable

---
+++ src/com/as/gof/behavioral/observer/myobservable/Main.java
@@ -0,0 +1,50 @@
+package com.as.gof.behavioral.observer.myobservable;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Created by Andrey Slesarchuk on 07/26/2020.
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    Subject subject = new Subject();
+    subject.subscribe(new Subscriber1());
+    subject.subscribe(new Subscriber2());
+    subject.notifySubscribers("Hello worlds!");
+  }
+
+  interface MyObservable {
+    void callMe(String msg);
+  }
+
+  static class Subject {
+    List<MyObservable> list = new ArrayList<>();
+
+    public void subscribe(MyObservable myObservable) {
+      list.add(myObservable);
+    }
+
+    public void unsubscribe(MyObservable myObservable) {
+      list.remove(myObservable);
+    }
+
+    public void notifySubscribers(String msg) {
+      list.forEach(item -> item.callMe(msg));
+    }
+
+  }
+
+  static class Subscriber1 implements MyObservable {
+    @Override public void callMe(String msg) {
+      System.out.println("s1 " + msg);
+    }
+  }
+
+  static class Subscriber2 implements MyObservable {
+    @Override public void callMe(String msg) {
+      System.out.println("s2 " + msg);
+    }
+  }
+
+}
