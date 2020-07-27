ab0873e2efb22e81f7f9e054da678339db4d31d1
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-07-27 22:50:12 +0300

    refactor

--- src/com/as/gof/behavioral/observer/newsagency/App.java
+++ src/com/as/gof/behavioral/observer/newsagency/App.java
@@ -13,7 +13,6 @@ public class App {
     observable.addObserver(a1);
     observable.setNews("news");
     //assertEquals(observer.getNews(), "news");
-
   }
 
 }
--- src/com/as/gof/behavioral/observer/newsagency/NewsAgency.java
+++ src/com/as/gof/behavioral/observer/newsagency/NewsAgency.java
@@ -28,4 +28,5 @@ public class NewsAgency {
       channel.update(this.news);
     }
   }
+  
 }
