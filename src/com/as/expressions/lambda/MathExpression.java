644bd18ff5c074fc83d3bfdcd17bc4c8e28c6c3a
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-10-19 16:04:36 +0300

    Lambda Expression refactored

--- src/com/as/expressions/lambda/MathExpression.java
+++ src/com/as/expressions/lambda/MathExpression.java
@@ -1,20 +1,24 @@
 package com.as.expressions.lambda;
-
+/**
+ * Created by Slesarchuk on 10/19/2020.
+ */
 public class MathExpression {
 
   public static void main(String args[]) {
     MathExpression tester = new MathExpression();
 
-    //with type declaration
+    // with type declaration
     MathOperation addition = (int a, int b) -> a + b;
 
-    //with out type declaration
+    // with out type declaration
     MathOperation subtraction = (a, b) -> a - b;
 
-    //with return statement along with curly braces
-    MathOperation multiplication = (int a, int b) -> { return a * b; };
+    // with return statement along with curly braces
+    MathOperation multiplication = (int a, int b) -> {
+      return a * b;
+    };
 
-    //without return statement and without curly braces
+    // without return statement and without curly braces
     MathOperation division = (int a, int b) -> a / b;
 
     System.out.println("10 + 5 = " + tester.operate(10, 5, addition));
@@ -22,13 +26,11 @@ public class MathExpression {
     System.out.println("10 x 5 = " + tester.operate(10, 5, multiplication));
     System.out.println("10 / 5 = " + tester.operate(10, 5, division));
 
-    //without parenthesis
-    GreetingService greetService1 = message ->
-        System.out.println("Hello " + message);
+    // without parenthesis
+    GreetingService greetService1 = message -> System.out.println("Hello " + message);
 
-    //with parenthesis
-    GreetingService greetService2 = (message) ->
-        System.out.println("Hello " + message);
+    // with parenthesis
+    GreetingService greetService2 = (message) -> System.out.println("Hello " + message);
 
     greetService1.sayMessage("Mike");
     greetService2.sayMessage("Andrey");
--- src/com/as/expressions/lambda/MyNumber.java
+++ src/com/as/expressions/lambda/MyNumber.java
@@ -1,19 +1,23 @@
 package com.as.expressions.lambda;
-
+/**
+ * Created by Slesarchuk on 10/19/2020.
+ */
 public class MyNumber {
   public static void main(String[] args) {
     ExpNumber myNumber1 = (i, t) -> {
       int a;
-      if (i<t) a = i;
-      else a=t;
+      if (i < t)
+        a = i;
+      else
+        a = t;
       return a;
     };
-    System.out.println(myNumber1.someNum(2,-3));
+    System.out.println(myNumber1.someNum(2, -3));
     ExpNumber myNumber2 = (i, t) -> i * 10;
     System.out.println(myNumber2.someNum(2, 7));
   }
 }
 
-interface  ExpNumber {
+interface ExpNumber {
   int someNum(int i, int t);
 }
