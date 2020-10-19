55bfc55a2a8d15006e8ddfae90d85709ac923f3d
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-10-19 15:52:01 +0300

    Update MyNumber.java

--- src/com/as/expressions/lambda/MyNumber.java
+++ src/com/as/expressions/lambda/MyNumber.java
@@ -2,13 +2,18 @@ package com.as.expressions.lambda;
 
 public class MyNumber {
   public static void main(String[] args) {
-    ExpNumber myNumber1 = (i) -> i * 5;
-    System.out.println(myNumber1.someNum(2));
-    ExpNumber myNumber2 = (i) -> i * 10;
-    System.out.println(myNumber2.someNum(2));
+    ExpNumber myNumber1 = (i, t) -> {
+      int a;
+      if (i<t) a = i;
+      else a=t;
+      return a;
+    };
+    System.out.println(myNumber1.someNum(2,-3));
+    ExpNumber myNumber2 = (i, t) -> i * 10;
+    System.out.println(myNumber2.someNum(2, 7));
   }
 }
 
 interface  ExpNumber {
-  int someNum(int i);
+  int someNum(int i, int t);
 }
