dd30c838c3b05ba796d2b8705ce6bab276fb9a05
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-10-20 11:51:30 +0300

    Update MyNumber.java
    myMethod with Consumer

--- src/com/as/expressions/lambda/MyNumber.java
+++ src/com/as/expressions/lambda/MyNumber.java
@@ -1,10 +1,23 @@
 package com.as.expressions.lambda;
+
+import java.util.function.Consumer;
+
 /**
  * Created by Slesarchuk on 10/19/2020.
  */
 public class MyNumber {
   public static void main(String[] args) {
-    ExpNumber myNumber1 = (i, t) -> {
+
+    Consumer<Object> myMethod = (n) -> {
+      if (n instanceof Integer) {
+        System.out.println("And this number - " + n + "!");
+      } else {
+        System.out.println("Hello Word and " + n + "!");
+        return;
+      }
+    };
+    
+    iNumber myNumber1 = (i, t) -> {
       int a;
       if (i < t)
         a = i;
@@ -12,12 +25,16 @@ public class MyNumber {
         a = t;
       return a;
     };
-    System.out.println(myNumber1.someNum(2, -3));
-    ExpNumber myNumber2 = (i, t) -> i * 10;
-    System.out.println(myNumber2.someNum(2, 7));
+    //System.out.println(myNumber1.someNum(2, -3));
+    myMethod.accept(myNumber1.someNum(2, -3));
+
+    iNumber myNumber2 = (i, t) -> i * 10;
+    myMethod.accept(myNumber2.someNum(5, 10));
+
+    myMethod.accept("All");
   }
 }
 
-interface ExpNumber {
+interface iNumber {
   int someNum(int i, int t);
 }
