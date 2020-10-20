525159134cba8d1a7b5a0f7243f23aee0e465341
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-10-20 12:09:14 +0300

    Update MyNumber.java

--- src/com/as/expressions/lambda/MyNumber.java
+++ src/com/as/expressions/lambda/MyNumber.java
@@ -8,7 +8,7 @@ import java.util.function.Consumer;
 public class MyNumber {
   public static void main(String[] args) {
 
-    Consumer<Object> myMethod = (n) -> {
+    Consumer<Object> myMethod = n -> {
       if (n instanceof Integer) {
         System.out.println("And this number - " + n + "!");
       } else {
@@ -16,8 +16,8 @@ public class MyNumber {
         return;
       }
     };
-    
-    iNumber myNumber1 = (i, t) -> {
+
+    ExpNumber myNumber1 = (i, t) -> {
       int a;
       if (i < t)
         a = i;
@@ -25,16 +25,16 @@ public class MyNumber {
         a = t;
       return a;
     };
-    //System.out.println(myNumber1.someNum(2, -3));
+
     myMethod.accept(myNumber1.someNum(2, -3));
 
-    iNumber myNumber2 = (i, t) -> i * 10;
+    ExpNumber myNumber2 = (i, t) -> i * 10;
     myMethod.accept(myNumber2.someNum(5, 10));
 
     myMethod.accept("All");
   }
 }
 
-interface iNumber {
+interface ExpNumber {
   int someNum(int i, int t);
 }
