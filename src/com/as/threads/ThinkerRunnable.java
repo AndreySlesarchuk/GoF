54e89203b81bd03701cc8a43009a00931434039b
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-10-24 18:35:49 +0300

    Create ThinkerRunnable.java

---
+++ src/com/as/threads/ThinkerRunnable.java
@@ -0,0 +1,21 @@
+package com.as.threads;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThinkerRunnable implements Runnable {
+
+  @Override
+  public void run() {
+    try {
+      TimeUnit.SECONDS.sleep(3);
+      System.out.println("Второй поток завершён");
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
+  }
+
+  public static void main(String[] args) {
+    new Thread(new ThinkerRunnable()).start();
+    System.out.println("Основной поток завершён");
+  }
+}
