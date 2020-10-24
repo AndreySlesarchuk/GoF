ffe8deba0a0dbc09bc431d6bdb25ce74699dbdd3
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-10-24 18:38:30 +0300

    Update ThinkerThread.java

--- src/com/as/threads/ThinkerThread.java
+++ src/com/as/threads/ThinkerThread.java
@@ -9,17 +9,17 @@ public class ThinkerThread extends Thread {
 
   @Override
   public void run() {
-    try {
-      TimeUnit.SECONDS.sleep(3); // аналогично Thread.sleep(3000L);
-      System.out.println("Второй поток завершён");
+    while (!Thread.currentThread().isInterrupted()) {
+      Math.cos(Integer.MAX_VALUE);
     }
-    catch (InterruptedException e) {
-      Thread.currentThread().interrupt();
-    }
+    Thread.currentThread().interrupt();
+    System.out.println("Второй поток завершён");
   }
 
   public static void main(String[] args) {
-    new ThinkerThread().start();
+    Thread thread = new ThinkerThread();
+    thread.start();
     System.out.println("Основной поток завершён");
+    thread.interrupt();
   }
 }
