d865ca00f03cbcde63c833cbe3beb593f1769234
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-10-24 18:36:27 +0300

    Update ThinkerRunnable.java

--- src/com/as/threads/ThinkerRunnable.java
+++ src/com/as/threads/ThinkerRunnable.java
@@ -2,6 +2,9 @@ package com.as.threads;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Created by Andrey Slesarchuk on 10/24/2020.
+ */
 public class ThinkerRunnable implements Runnable {
 
   @Override
@@ -9,7 +12,8 @@ public class ThinkerRunnable implements Runnable {
     try {
       TimeUnit.SECONDS.sleep(3);
       System.out.println("Второй поток завершён");
-    } catch (InterruptedException e) {
+    }
+    catch (InterruptedException e) {
       Thread.currentThread().interrupt();
     }
   }
