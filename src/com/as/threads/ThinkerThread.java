7bd17ebd805b0b80c9f37c2bcbc47ea5a80d335c
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-10-25 17:40:39 +0300

    Update ThinkerThread.java

--- src/com/as/threads/ThinkerThread.java
+++ src/com/as/threads/ThinkerThread.java
@@ -1,9 +1,14 @@
 package com.as.threads;
 
+import com.as.expressions.lambda.MyNumber;
+import org.apache.log4j.Logger;
+
 /**
  * Created by Andrey Slesarchuk on 10/24/2020.
  */
 public class ThinkerThread extends Thread {
+
+  static Logger log = Logger.getLogger(ThinkerThread.class);
 
   @Override
   public void run() {
@@ -11,13 +16,13 @@ public class ThinkerThread extends Thread {
       Math.cos(Integer.MAX_VALUE);
     }
     Thread.currentThread().interrupt();
-    System.out.println("Второй поток завершён");
+    log.info("Второй поток завершён");
   }
 
   public static void main(String[] args) {
     Thread thread = new ThinkerThread();
     thread.start();
-    System.out.println("Основной поток завершён");
+    log.info("Основной поток завершён");
     thread.interrupt();
   }
 }
