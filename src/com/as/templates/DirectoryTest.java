fbeebe673bc38d060c2d293c30dcdfe22dc2fac2
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-10-26 11:54:57 +0300

    Create DirectoryTest.java

---
+++ src/com/as/templates/DirectoryTest.java
@@ -0,0 +1,49 @@
+package com.as.templates;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import org.apache.log4j.Logger;
+
+public class DirectoryTest {
+
+  static Logger log = Logger.getLogger(DirectoryTest.class);
+
+  public static void main(String[] args) {
+    Path path = Paths.get("d:/__downloads");
+    Long dirSize = getDirectorySizeJava8(path);
+    log.info(dirSize);
+  }
+
+
+  // size of directory in bytes
+  public static long getDirectorySizeJava8(Path path) {
+    long size = 0;
+    // need close Files.walk
+    try (Stream<Path> walk = Files.walk(path)) {
+
+      size = walk
+          // .peek(System.out::println) // debug
+          .filter(Files::isRegularFile).mapToLong(p -> {
+            // ugly, can pretty it with an extract method
+            try {
+              return Files.size(p);
+            }
+            catch (IOException e) {
+              System.out.printf("Failed to get size of %s%n%s", p, e);
+              return 0L;
+            }
+          }).sum();
+    }
+    catch (IOException e) {
+      System.out.printf("IO errors %s", e);
+    }
+
+    return size;
+
+  }
+
+}
