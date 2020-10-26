bc83536a10feeb7f37680049e413bfa6de9ec98b
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-10-26 12:41:10 +0300

    added dir methods

--- src/com/as/directories/DirectoryTest.java
+++ src/com/as/directories/DirectoryTest.java
@@ -1,11 +1,13 @@
 package com.as.directories;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 
 /**
@@ -19,7 +21,14 @@ public class DirectoryTest {
   public static void main(String[] args) {
     Path path = Paths.get("d:/__downloads");
     Long dirSize = getDirectorySizeJava8(path);
-    log.info("Directory " + path.toString() + " Size : " + new ConvertSize().getSize(dirSize));
+    log.info("Directory " + path.toString() + " Size : " + new ConvertSize().getSize(dirSize)+ " - " + dirSize);
+
+    dirSize = FileUtils.sizeOfDirectory(path.toFile());
+    log.info("(FileUtils) Directory " + path.toString() + " Size : " + new ConvertSize().getSize(dirSize) + " - " + dirSize);
+
+    dirSize = getDirectorySizeLegacy(path.toFile());
+    log.info("(Legacy) Directory " + path.toString() + " Size : " + new ConvertSize().getSize(dirSize) + " - " + dirSize);
+
   }
 
   // size of directory in bytes
@@ -27,7 +36,6 @@ public class DirectoryTest {
     long size = 0;
     // need close Files.walk
     try (Stream<Path> walk = Files.walk(path)) {
-
       size = walk
           // .peek(System.out::println) // debug
           .filter(Files::isRegularFile).mapToLong(p -> {
@@ -44,8 +52,23 @@ public class DirectoryTest {
     catch (IOException e) {
       System.out.printf("IO errors %s", e);
     }
-
     return size;
+
+  }
+
+  public static long getDirectorySizeLegacy(File dir) {
+
+    long length = 0;
+    File[] files = dir.listFiles();
+    if (files != null) {
+      for (File file : files) {
+        if (file.isFile())
+          length += file.length();
+        else
+          length += getDirectorySizeLegacy(file);
+      }
+    }
+    return length;
 
   }
 
