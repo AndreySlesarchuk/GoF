d50f7cd692dec4edb65d9d34091474e6af4656ad
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-10-28 17:51:00 +0300

    code refactored

--- src/com/as/java8/Java8Function2.java
+++ src/com/as/java8/Java8Function2.java
@@ -5,15 +5,12 @@ import java.util.function.Function;
 /**
  * Created by Andrey Slesarchuk on 10/28/2020.
  */
-public class Java2Function {
+public class Java8Function2 {
   public static void main(String[] args) {
 
     Function<String, Integer> func = x -> x.length();
-
     Function<Integer, Integer> func2 = x -> x * 2;
-
-    Integer result = func.andThen(func2).apply("andrey");   // 6
-
+    Integer result = func.andThen(func2).apply("andrey"); // 6
     System.out.println(result);
 
   }
