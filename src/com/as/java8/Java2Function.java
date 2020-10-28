2c937a619ed324645afaffdb02995a1384ec6d2f
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-10-28 17:34:29 +0300

    up1

--- src/com/as/java8/Java2Function.java
+++ src/com/as/java8/Java2Function.java
@@ -1,7 +1,18 @@
 package com.as.java8;
 
+import java.util.function.Function;
+
 /**
  * Created by Andrey Slesarchuk on 10/28/2020.
  */
 public class Java2Function {
+  public static void main(String[] args) {
+
+    Function<String, Integer> func = x -> x.length();
+
+    Integer apply = func.apply("andrey");   // 6
+
+    System.out.println(apply);
+
+  }
 }
