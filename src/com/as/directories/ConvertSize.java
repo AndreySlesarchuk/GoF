c9cef685db52b3ab412b138d777137f319876317
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-10-30 12:44:00 +0300

    Update ConvertSize.java

--- src/com/as/directories/ConvertSize.java
+++ src/com/as/directories/ConvertSize.java
@@ -6,23 +6,20 @@ package com.as.directories;
 public class ConvertSize {
 
   public String getSize(Long sizeBytes) {
-    String cntSize;
-
     long sizeKb = sizeBytes / 1024;
     long sizeMb = sizeKb / 1024;
     long sizeGb = sizeMb / 1024;
 
     if (sizeGb > 0) {
-      cntSize = sizeGb + " GB";
+      return sizeGb + " GB";
     }
     else
       if (sizeMb > 0) {
-        cntSize = sizeMb + " MB";
+        return sizeMb + " MB";
       }
       else {
-        cntSize = sizeKb + " KB";
+        return sizeKb + " KB";
       }
-    return cntSize;
   }
 
 }
