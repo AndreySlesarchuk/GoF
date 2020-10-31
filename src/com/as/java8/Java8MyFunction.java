c78002cb702948eae5c1fd53d93f61d636f69cbd
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-10-31 16:53:51 +0300

    Update Java8MyFunction.java

--- src/com/as/java8/Java8MyFunction.java
+++ src/com/as/java8/Java8MyFunction.java
@@ -1,14 +1,24 @@
 package com.as.java8;
 
+import java.util.function.Consumer;
 import java.util.function.Predicate;
-
+/**
+ * Created by Andrey Slesarchuk on 10/31/2020.
+ */
 public class Java8MyFunction {
 
   public static void main(String[] args) {
+
+    //Predicate
     Predicate<Integer> isEvenNumber = x -> x % 2==0;
 
     System.out.println(isEvenNumber.test(4));
     System.out.println(isEvenNumber.test(3));
+
+    //Consumer
+    Consumer greetings = x -> System.out.println("Hello " + x + " !!!");
+    greetings.accept("Elena");
+    System.out.println(greetings);
   }
 
 }
