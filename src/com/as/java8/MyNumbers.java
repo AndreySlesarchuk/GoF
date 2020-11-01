8c6fe40dfb5e8ae06c79a85e2749d7a5e1d6afe3
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-11-01 17:56:07 +0300

    Update MyNumbers.java

--- src/com/as/java8/MyNumbers.java
+++ src/com/as/java8/MyNumbers.java
@@ -2,12 +2,17 @@ package com.as.java8;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.function.Consumer;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by Andrey Slesarchuk on 11/01/2020.
  */
 public class MyNumbers {
+
+  public interface Function<F, T> {
+    T apply(F from);
+  }
 
   public static void main(String[] args) {
     ArrayList<Integer> numbers = new ArrayList<Integer>();
@@ -17,7 +22,16 @@ public class MyNumbers {
     numbers.add(1);
     MyNumbers myNumbers = new MyNumbers();
     System.out.println(myNumbers.joinNumbers(numbers));
+
+    myNumbers.joinNumbersF(numbers);
+
   }
+
+  Function<Integer, String> intToString = new Function<Integer, String>() {
+    @Override public String apply(Integer from) {
+      return from.toString();
+    }
+  };
 
   public String joinNumbers(Collection<? extends Integer> numbers) {
     StringBuilder result = new StringBuilder();
@@ -28,6 +42,30 @@ public class MyNumbers {
       else
         result.append(", ");
       result.append(number);
+    }
+    return result.toString();
+  }
+
+  public String joinNumbersF(Collection<? extends Integer> numbers) {
+    return join(map(numbers, intToString), "|");
+  }
+
+  public static <F, T> List<T> map(Collection<F> from, Function<? super F,? extends T> transformer) {
+    ArrayList<T> result = new ArrayList<T>();
+    for (F element : from)
+      result.add(transformer.apply(element));
+    return result;
+  }
+
+  public static <T> String join(Collection<T> from, String separator) {
+    StringBuilder result = new StringBuilder();
+    boolean first = true;
+    for (T element : from) {
+      if (first)
+        first = false;
+      else
+        result.append(separator);
+      result.append(element);
     }
     return result.toString();
   }
