d13f5d2e121bb76fd6bdf171b178ab1cddf3d5ae
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-11-04 17:55:06 +0300

    sorting

---
+++ src/com/as/expressions/lambda/sorting/Developer.java
@@ -0,0 +1,40 @@
+package com.as.expressions.lambda.sorting;
+
+import java.math.BigDecimal;
+
+public class Developer {
+  String name;
+  BigDecimal salary;
+  Integer age;
+
+  public Developer(String name, BigDecimal salary, int age) {
+    this.name = name;
+    this.salary = salary;
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public BigDecimal getSalary() {
+    return salary;
+  }
+
+  public void setSalary(BigDecimal salary) {
+    this.salary = salary;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+}
