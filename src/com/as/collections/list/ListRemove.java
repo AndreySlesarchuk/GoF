33284e2de1ae1911631039d0dd693c04fd0f2e15
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-11-08 19:15:17 +0300

    Update ListRemove.java

--- src/com/as/collections/list/ListRemove.java
+++ src/com/as/collections/list/ListRemove.java
@@ -1,7 +1,9 @@
 package com.as.collections.list;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import org.junit.Test;
 
@@ -10,14 +12,12 @@ import org.junit.Test;
  */
 public class ListRemove {
 
-  @Test
-  public static void removeAll1(List<Integer> list, int element) {
+  public void removeAll1(List<Integer> list, int element) {
     while (list.contains(element)) {
       list.remove(element);
     }
   }
 
-  @Test
   public static void removeAll2(List<Integer> list, int element) {
     while (list.contains(element)) {
       list.remove(element);
@@ -25,11 +25,22 @@ public class ListRemove {
   }
 
   public static void main(String[] args) {
-    //given
-    List<Integer> list = Arrays.asList(1, 2, 3);
+    List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3));
     int valueToRemove = 1;
+    for (int i = 0; i < list.size(); i++) {
+      if (Objects.equals(valueToRemove, list.get(i))) {
+        list.remove(i);
+        i--;
+      }
+    }
+    //given
+    //List<Integer> list = Arrays.asList(1, 2, 3);
+    //int valueToRemove = 1;
+
+    //list.remove(valueToRemove);
+
     //when
-    removeAll2(list, valueToRemove);
+    //removeAll2(list, valueToRemove);
     System.out.println(list.toString());
     //then
     //assertThat(list).isEqualTo(list(2, 3));
