575507ba72200a7e6040012c39edad173d7ef3a0
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-11-21 18:13:12 +0300

    logicTest added

---
+++ src/com/as/java8/logicTest.java
@@ -0,0 +1,21 @@
+package com.as.java8;
+
+/**
+ * Created by Andrey Slesarchuk on 11/21/2020.
+ */
+public class logicTest {
+
+  public static void main(String[] args) {
+
+    if ((company.isEmailExpired()  && !password) || (!company.hasEmailAmount())) {
+      System.out.println("Email expired or ran out of balance");
+    } else {
+      System.out.println(myNumbers.joinNumbersF(numbers));
+    }
+
+
+
+  }
+
+
+}
