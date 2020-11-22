7a2c9e541c33c1c49181c2b433d6c2024d8f3a73
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-11-22 17:33:04 +0300

    Update ListRemove.java

--- src/com/as/collections/list/ListRemove.java
+++ src/com/as/collections/list/ListRemove.java
@@ -47,25 +47,28 @@ public class ListRemove {
 
   public static void main(String[] args) {
     Watch watch = new Watch();
-    List<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+    List<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
     int valueToRemove = 8;
 
     removeFor(list1, valueToRemove);
-    watch.totalTime("List removeFor time = "); //1 - 333 260 107 243 642, 2 - 333 674 579 937 381
+    watch.totalTime("List removeFor time = "); // 1 - 333 260 107 243 642, 2 -
+                                               // 333 674 579 937 381
     System.out.println("First - " + Optional.ofNullable(list1.toString()).orElse("nothing"));
 
-    List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+    List<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
     watch = new Watch();
     removeWithIterator(list2, valueToRemove);
-    watch.totalTime("List removeWithIterator time = "); //1 - 334 319 733 627 250, 2 - 334 217 091 777 304
+    watch.totalTime("List removeWithIterator time = "); // 1 - 334 319 733 627
+                                                        // 250, 2 - 334 217 091
+                                                        // 777 304
     System.out.println("Second - " + Optional.ofNullable(list2.toString()).orElse("nothing"));
 
-    //list = new ArrayList<>(Arrays.asList(4, 5, 6));
-    //removeAll2(list, valueToRemove);
-    //System.out.println("Second - " + list.toString());
+    // list = new ArrayList<>(Arrays.asList(4, 5, 6));
+    // removeAll2(list, valueToRemove);
+    // System.out.println("Second - " + list.toString());
 
-    //then
-    //assertThat(list).isEqualTo(list(2, 3));
+    // then
+    // assertThat(list).isEqualTo(list(2, 3));
   }
 
 }
