f35a704751a34a071857d32fcab60fecd828588c
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-11-22 17:29:28 +0300

    Update Watch.java

--- src/com/as/utils/Watch.java
+++ src/com/as/utils/Watch.java
@@ -1,18 +1,23 @@
 package com.as.utils;
-
+/**
+ * Created by Andrey Slesarchuk on 11/22/2020.
+ */
 public class Watch {
-    private long startTime;
-    private long endTime;
-    public void start() {
-      startTime = System.nanoTime();
-    }
-    private void stop() {
-      endTime = System.nanoTime();
-    }
-    public void totalTime(String s) {
-      stop();
-      System.out.println(s + (endTime - startTime));
-      System.out.println(s + ((endTime - startTime)/1000000));//Milliseconds
-    }
+  private long startTime;
+
+  private long endTime;
+
+  public void start() {
+    startTime = System.nanoTime();
+  }
+
+  private void stop() {
+    endTime = System.nanoTime();
+  }
+
+  public void totalTime(String s) {
+    stop();
+    System.out.println(s + (endTime - startTime));
+    //System.out.println(s + ((endTime - startTime) / 1000000));// Milliseconds
   }
 }
