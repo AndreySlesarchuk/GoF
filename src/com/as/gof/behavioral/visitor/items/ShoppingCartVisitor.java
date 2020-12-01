db2cf06232743cfb0adb9f7aefc9b85d2afb35fc
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-12-01 16:24:44 +0300

    Update ShoppingCartVisitor.java

--- src/com/as/gof/behavioral/visitor/items/ShoppingCartVisitor.java
+++ src/com/as/gof/behavioral/visitor/items/ShoppingCartVisitor.java
@@ -1,8 +1,10 @@
 package com.as.gof.behavioral.visitor.items;
+
 /**
  * Created by Andrey Slesarchuk on 11/29/2020.
  */
 public interface ShoppingCartVisitor {
   int visit(Book book);
+
   int visit(Fruit fruit);
 }
