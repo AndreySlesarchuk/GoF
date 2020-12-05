4f8d9047e43b4650d6c3a3b2ea5b4e4ea98c7422
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-12-05 18:26:47 +0300

    Update Admin.java

--- src/com/as/gof/behavioral/observer/chat/Admin.java
+++ src/com/as/gof/behavioral/observer/chat/Admin.java
@@ -8,7 +8,8 @@ import java.util.logging.Logger;
 public class Admin implements ChatObserver {
   private static final Logger LOGGER = Logger.getLogger(Admin.class.getName());
 
-  @Override public void update(String message) {
+  @Override
+  public void update(String message) {
     LOGGER.info(String.format("%s get new message: %s", this.getClass().getName(), message));
   }
 }
