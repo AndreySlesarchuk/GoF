9c88757a3769474cbae4096732b3a816bf679ced
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-12-05 18:24:34 +0300

    Update User.java

--- src/com/as/gof/behavioral/observer/chat/User.java
+++ src/com/as/gof/behavioral/observer/chat/User.java
@@ -8,7 +8,8 @@ import java.util.logging.Logger;
 public class User implements ChatObserver {
   private static final Logger LOGGER = Logger.getLogger(User.class.getName());
 
-  @Override public void update(String message) {
+  @Override
+  public void update(String message) {
     LOGGER.info(String.format("%s get new message: %s", this.getClass().getName(), message));
   }
 }
