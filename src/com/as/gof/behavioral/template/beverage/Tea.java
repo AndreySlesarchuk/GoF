e305a9363fd43a7e9f438b656b24004788dea6d4
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-12-06 18:03:12 +0300

    Update Tea.java

--- src/com/as/gof/behavioral/template/beverage/Tea.java
+++ src/com/as/gof/behavioral/template/beverage/Tea.java
@@ -1,13 +1,16 @@
 package com.as.gof.behavioral.template.beverage;
+
 /**
  * Created by Andrey Slesarchuk on 06/19/2020.
  */
 public class Tea extends Beverage {
-  @Override void addBeverage() {
+  @Override
+  void addBeverage() {
     System.out.println("add tea");
   }
 
-  @Override void addConditment() {
+  @Override
+  void addConditment() {
     System.out.println("add lemon");
   }
 }
