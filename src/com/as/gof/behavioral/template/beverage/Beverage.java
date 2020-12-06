6c073d646ef3a86254ea0cabdbe4827bb6bf86ca
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-12-06 18:01:23 +0300

    Update Beverage.java

--- src/com/as/gof/behavioral/template/beverage/Beverage.java
+++ src/com/as/gof/behavioral/template/beverage/Beverage.java
@@ -1,4 +1,5 @@
 package com.as.gof.behavioral.template.beverage;
+
 /**
  * Created by Andrey Slesarchuk on 06/19/2020.
  */
@@ -13,16 +14,22 @@ abstract class Beverage {
     endHook();
   }
 
-  void startHook() {}
-  void endHook() {}
+  void startHook() {
+  }
+
+  void endHook() {
+  }
 
   private void boilWater() {
     System.out.println("boiled water");
   }
+
   private void addSugar() {
     System.out.println("add sugar");
   }
+
   abstract void addBeverage();
+
   abstract void addConditment();
 
 }
