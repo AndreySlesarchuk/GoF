6ee3272d28b646f595523f8a1115e97dd4c04f5b
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-12-06 18:01:56 +0300

    Update Coffee.java

--- src/com/as/gof/behavioral/template/beverage/Coffee.java
+++ src/com/as/gof/behavioral/template/beverage/Coffee.java
@@ -1,22 +1,27 @@
 package com.as.gof.behavioral.template.beverage;
+
 /**
  * Created by Andrey Slesarchuk on 06/19/2020.
  */
 public class Coffee extends Beverage {
 
-  @Override void addBeverage() {
+  @Override
+  void addBeverage() {
     System.out.println("add coffee");
   }
 
-  @Override void addConditment() {
+  @Override
+  void addConditment() {
     System.out.println("add milk");
   }
 
-  @Override void startHook() {
+  @Override
+  void startHook() {
     System.out.println("wash the cup");
   }
 
-  @Override void endHook() {
+  @Override
+  void endHook() {
     System.out.println("bill");
   }
 }
