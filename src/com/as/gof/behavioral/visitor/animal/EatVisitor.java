97dbf77fff5faefd7ab51b33172caa1c8af0fbcc
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-12-08 23:37:12 +0300

    Update EatVisitor.java

--- src/com/as/gof/behavioral/visitor/animal/EatVisitor.java
+++ src/com/as/gof/behavioral/visitor/animal/EatVisitor.java
@@ -1,20 +1,21 @@
 package com.as.gof.behavioral.visitor.animal;
+
 /**
  * Created by Andrey Slesarchuk on 05/25/2020.
  */
-public class EatVisitor implements AnimalVisitor{
-    @Override
-    public void action(Cat cat) {
-        System.out.println("eat fish");
-    }
+public class EatVisitor implements AnimalVisitor {
+  @Override
+  public void action(Cat cat) {
+    System.out.println("eat fish");
+  }
 
-    @Override
-    public void action(Dog dog) {
-        System.out.println("eat meat");
-    }
+  @Override
+  public void action(Dog dog) {
+    System.out.println("eat meat");
+  }
 
-    @Override
-    public void action(Chicken chicken) {
-        System.out.println("eat corn");
-    }
+  @Override
+  public void action(Chicken chicken) {
+    System.out.println("eat corn");
+  }
 }
