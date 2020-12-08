d5c10df90055b07676b44029e37aee209803b8f8
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-12-08 23:35:21 +0300

    Update Cat.java

--- src/com/as/gof/behavioral/visitor/animal/Cat.java
+++ src/com/as/gof/behavioral/visitor/animal/Cat.java
@@ -1,10 +1,11 @@
 package com.as.gof.behavioral.visitor.animal;
+
 /**
  * Created by Andrey Slesarchuk on 05/25/2020.
  */
-public class Cat implements Animal{
-    @Override
-    public void accept(AnimalVisitor animalVisitor) {
-        animalVisitor.action(this);
-    }
+public class Cat implements Animal {
+  @Override
+  public void accept(AnimalVisitor animalVisitor) {
+    animalVisitor.action(this);
+  }
 }
