1dfb93ce63fee9b6564b2e4770285812420410c3
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-12-08 23:36:56 +0300

    Update Dog.java

--- src/com/as/gof/behavioral/visitor/animal/Dog.java
+++ src/com/as/gof/behavioral/visitor/animal/Dog.java
@@ -1,10 +1,11 @@
 package com.as.gof.behavioral.visitor.animal;
+
 /**
  * Created by Andrey Slesarchuk on 05/25/2020.
  */
-public class Dog implements Animal{
-    @Override
-    public void accept(AnimalVisitor animalVisitor) {
-        animalVisitor.action(this);
-    }
+public class Dog implements Animal {
+  @Override
+  public void accept(AnimalVisitor animalVisitor) {
+    animalVisitor.action(this);
+  }
 }
