5b6eebbcf52324b0867bb5e6be1d2217e6a96a04
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-12-08 23:36:05 +0300

    Update Chicken.java

--- src/com/as/gof/behavioral/visitor/animal/Chicken.java
+++ src/com/as/gof/behavioral/visitor/animal/Chicken.java
@@ -1,10 +1,11 @@
 package com.as.gof.behavioral.visitor.animal;
+
 /**
  * Created by Andrey Slesarchuk on 05/25/2020.
  */
-public class Chicken implements Animal{
-    @Override
-    public void accept(AnimalVisitor animalVisitor) {
-        animalVisitor.action(this);
-    }
+public class Chicken implements Animal {
+  @Override
+  public void accept(AnimalVisitor animalVisitor) {
+    animalVisitor.action(this);
+  }
 }
