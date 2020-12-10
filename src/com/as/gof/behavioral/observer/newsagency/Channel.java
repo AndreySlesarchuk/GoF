3b999e1dfc6f2ed96783c4f278206a1ee829f27b
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-12-10 18:57:06 +0300

    Update Channel.java

--- src/com/as/gof/behavioral/observer/newsagency/Channel.java
+++ src/com/as/gof/behavioral/observer/newsagency/Channel.java
@@ -4,6 +4,5 @@ package com.as.gof.behavioral.observer.newsagency;
  * Created by Andrey Slesarchuk on 07/26/2020.
  */
 public interface Channel {
-
-  public void update(Object o);
+  void update(Object o);
 }
