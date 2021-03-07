c8e206c75750f3ff947b2d1a74fa41d43f882b3b
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2021-03-07 19:08:44 +0300

    TestClass added

--- src/com/as/other/list/ArrayExamples.java
+++ src/com/as/other/list/ArrayExamples.java
@@ -19,12 +19,14 @@ public class ArrayExamples {
     watch.start();
     Solution solution = new Solution();
 
-    watch.totalTime("Result reverse: " + solution.reverse(2048) + " Time: ");
+    watch.totalTime("Result reverse: " + solution.reverse(48) + " Time: ");
     watch.totalTime("Result runningSum : " + solution.runningSum(m1) + " Time: ");
     watch.totalTime("Result1: " + solution.twoSum1(m1, t1) + " Time: ");
     watch.totalTime("Result2: " + solution.twoSum2(m2, t2) + " Time: ");
 
   }
+
+
 
   class Solution {
 
