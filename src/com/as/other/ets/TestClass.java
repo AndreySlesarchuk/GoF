36933e3f2167df4ef8faf680d66a1a553de712ba
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2021-03-07 19:48:18 +0300

    TestClass tested

--- src/com/as/other/ets/TestClass.java
+++ src/com/as/other/ets/TestClass.java
@@ -1,14 +1,21 @@
 package com.as.other.ets;
 
-public class TestClass{
-  public static int getSwitch(String str){
-    return (int) Math.round( Double.parseDouble(str.substring(1, str.length()-1)) );
+public class TestClass {
+  public static int getSwitch(String str) {
+    return (int) Math.round(Double.parseDouble(str.substring(1, str.length() - 1)));
   }
-  public static void main(String args []){
-    switch(getSwitch(args[0])){
-      case 0 : System.out.print("Hello ");
-      case 1 : System.out.print("World"); break;
-      default : System.out.print("Good Bye");
+
+  public static void main(String args[]) {
+    String t1 = "--0.50";
+    int t2 = getSwitch(t1);
+    switch (t2) {
+      case 0:
+        System.out.print("Hello ");
+      case 1:
+        System.out.print("World");
+        break;
+      default:
+        System.out.print("Good Bye");
     }
   }
 }
