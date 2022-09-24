c969518dc43ca863b83ff4d12536a46aa779ea5f
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2022-09-24 18:39:13 +0300

    initial commit

--- src/com/as/gof/behavioral/visitor/animal/Animal.java
+++
@@ -1,8 +0,0 @@
-package com.as.gof.behavioral.visitor.animal;
-
-/**
- * Created by Andrey Slesarchuk on 05/25/2020.
- */
-public interface Animal {
-  void accept(AnimalVisitor animalVisitor);
-}
--- src/com/as/other/interfaces/B.java
+++
@@ -1,7 +0,0 @@
-package com.as.other.interfaces;
-
-public interface B {
-  default void foo() {
-    System.out.println("Foo B");
-  }
-}
